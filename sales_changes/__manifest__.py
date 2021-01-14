4c9ea35cf1bef5dce06ee40f9253dee4da059cef
ebs@odoosh123 <ebs.odoo.sh@gmail.com>
2021-01-14 15:29:48 +0200

    [Change] sales_changes/__manifest__.py

--- sales_changes/__manifest__.py
+++ sales_changes/__manifest__.py
@@ -9,8 +9,8 @@
     'website': "https://www.everbsgroup.com/",
     'depends': ['sale'],
     'data': [
-        'data/mail_data_changes.xml',
         'report/sale_report_template_changes.xml',
+        'data/mail_data_changes.xml',
         'view/sale_order_form_changes.xml'
     ],
     'auto_install': False,
